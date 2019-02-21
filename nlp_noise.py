4db73ffdefea8db862fd46016356ea1f53cf2d5b
gayathrivs7 <gaya3vs.0007@gmail.com>
2019-02-21 15:28:53 +0000

    File 1 nlp

---
+++ nlp_noise.py
@@ -0,0 +1,11 @@
+#prg to remove stopwords 
+noise_list = [ "is", "a", " this",".","of","in","an","the","as","but","was"]
+
+def remove_noise(input_text):
+    words=input_text.split()
+    noise_free_words=[word for word in words if word not in noise_list]
+    noise_free_text = " ".join(noise_free_words)
+    return noise_free_text
+
+text=remove_noise("this is a sample text of .  gayathri was in ")
+print(text)
