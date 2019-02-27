1db75cefd6446b94773712c5a6c1f48228ac2678
gayathrivs7 <gaya3vs.0007@gmail.com>
2019-02-27 12:31:25 +0000

    Learning to tokenize

--- nlp_college.py
+++ nlp_college.py
@@ -5,4 +5,11 @@ from nltk.tokenize import word_tokenize
 #function to split text into word
 
 tokens = word_tokenize("The quick brown fox jumps over the lazy dog")
+#========================================================================
 
+#correct
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+nltk.download('punkt')
+data = "All work and no play makes jack a dull boy, all work and no play"
+print(word_tokenize(data))
