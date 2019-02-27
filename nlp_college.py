dadae4dbb45b92ded4395d02595add24d57c4bc3
gayathrivs7 <gaya3vs.0007@gmail.com>
2019-02-27 12:33:06 +0000

    Learning to tokenize

--- nlp_college.py
+++ nlp_college.py
@@ -13,3 +13,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 nltk.download('punkt')
 data = "All work and no play makes jack a dull boy, all work and no play"
 print(word_tokenize(data))
+#======================================
+from nltk.tokenize import sent_tokenize, word_tokenize
+data = "All work and no play makes jack a dull boy, all work and no play"
+print(word_tokenize(data))
