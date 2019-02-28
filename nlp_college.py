fe20a9404fbea30620af4d3d587996ea4c38e3f4
gayathrivs7 <gaya3vs.0007@gmail.com>
2019-02-28 11:15:41 +0000

    Stoword removal

--- nlp_college.py
+++ nlp_college.py
@@ -20,3 +20,25 @@ print(word_tokenize(data))
 
 data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
 print(sent_tokenize(data))
+#===========================================
+
+ 
+data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
+ 
+phrases = sent_tokenize(data)
+words = word_tokenize(data)
+ 
+print(phrases)
+print(words)
+#==================================Stopwords removal================
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
+stopWords = set(stopwords.words('english')) 
+print(stopWords)
+words = word_tokenize(data)
+wordsFiltered = []
+for w in words:
+    if w not in stopWords:
+        wordsFiltered.append(w)
+print(wordsFiltered)
