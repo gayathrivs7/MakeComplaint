ae4f67896e1d79a7962abd0f6ab4ccd0896b18a5
Gayathri <gaya3vs.0007@gmail.com>
2019-04-11 15:43:22 +0530

    Classing : punctuate fn done

--- Complaint.py
+++ Complaint.py
@@ -1,17 +1,27 @@
 #main program
 import spacy
 import pandas as pd
+from preprocess import punctuate #preprocess.py
+import preprocess
+
 
 class Complaint:
-
+    
     
 
     def __init__(self,dataset,nlp):
         #/home/gayathri/project/MakeComplaint/data.csv
         self.dataset=dataset
+
         #print(self.dataset)
         self.dataset= pd.read_csv(self.dataset)
         self.nlp =nlp
+
+     #department headings class names.
+    def department_class(self):
+        datasets=self.dataset
+        departments=self.dataset['Departments'].unique()
+        return departments,datasets
 
 
 
@@ -22,6 +32,8 @@ file =   '/home/gayathri/project/MakeComplaint/data.csv'
 nlp = spacy.load('en_core_web_md')
          
 c = Complaint(file,nlp)
+category,dataset = c.department_class()
+preprocess.punctuate(dataset)
 
     
 
--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,22 @@
-class Preprocess:
-    pass
+
+
+
+def punctuate(dataset):
+    punctuations = '''!()-[]{};:'"\,.<>/?@#$%^&*_~'''
+    text = dataset['Subject']
+
+       
+
+    # remove punctuation from the string
+    no_punct = ""
+    no_punctuate =[]
+    for char in  dataset['Subject']:
+
+        if char not in punctuations:
+            no_punct = no_punct + char
+            no_punctuate.append(no_punct)
+
+        # display the unpunctuated string
+    #print(no_punctuate)
+    return(no_punctuate)
+
