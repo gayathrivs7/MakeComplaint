0514b133bf013f8907ec86a083a19a396030e108
Gayathri <gaya3vs.0007@gmail.com>
2019-04-11 16:50:29 +0530

    Class Dataframes

--- prj/Complaint.py
+++ prj/Complaint.py
@@ -1,11 +1,46 @@
 #main program
-import init
 import spacy
+import pandas as pd
+import preprocess
+from preprocess import data_clean #preprocess.py
+import dataframes
+from dataframes import dataframing
+
+
 
 class Complaint:
-    file =   '/home/gayathri/project/MakeComplaint/data.csv'   
-    nlp = spacy.load('en_core_web_md')
+    
+    
 
-    init.init(file,nlp)
+    def __init__(self,dataset,nlp):
+        #/home/gayathri/project/MakeComplaint/data.csv
+        self.dataset=dataset
+
+        #print(self.dataset)
+        self.dataset= pd.read_csv(self.dataset)
+        self.nlp =nlp
+
+     #department headings class names.
+    def department_class(self):
+        datasets=self.dataset
+        departments=self.dataset['Departments'].unique()
+        return departments,datasets
+
+
+
+
+
+ #Execution begins
+file =   '/home/gayathri/project/MakeComplaint/data.csv'   
+nlp = spacy.load('en_core_web_md')
+         
+c = Complaint(file,nlp)
+
+category,dataset = c.department_class()
+
+dataset = preprocess.data_clean(dataset)
+
+dfwater,dfpwd,dfksrtc,dfkseb,dfenv = dataframes.dataframing(dataset)
+
     
 
---
+++ prj/preprocess.py
@@ -0,0 +1,19 @@
+
+def data_clean(dataset):
+  
+    # unpunctuate and lower case
+    dataset['Subject'] = dataset['Subject'].str.replace('[^\w\s]','').str.lower()
+    dataset['Complaint'] = dataset['Complaint'].str.replace('[^\w\s]','').str.lower()
+
+    dataset['Complaint'] =  dataset['Complaint'].str.replace(',',' ').str.lower() 
+    
+    dataset['Subject'] =  dataset['Subject'] .str.replace('\d+', ' ')
+    dataset['Complaint'] =  dataset['Complaint'] .str.replace('\d+', ' ')
+    dataset['Subject'] =  dataset['Subject'].str.rstrip('\n')
+    print(dataset['Complaint'])
+    return dataset
+
+
+    
+
+    
