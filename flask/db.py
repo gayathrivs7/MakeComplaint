98203376d9122fc216d15832f5d1444e62578c1f
Gayathri <gaya3vs.0007@gmail.com>
2019-04-25 12:43:24 +0530

    SQL Alchemy learning

---
+++ flask/db.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
