37fc686704a9711ea92c689379d2eb468cc281b5
Gayathri <gaya3vs.0007@gmail.com>
2019-04-25 17:33:03 +0530

    SQLALchemy

---
+++ prj/dbfile.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+
+app = Flask('__name__')
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI']        = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
+
+db = SQLAlchemy(app)
+
+Migrate(app,db)
+
+
+
