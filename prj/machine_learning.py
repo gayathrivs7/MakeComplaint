0c130aa59480bb6f1b0e26ccb44fc8a130e2f198
Gayathri <gaya3vs.0007@gmail.com>
2019-05-01 18:09:49 +0530

    machine learnining on nlp

--- prj/machine_learning.py
+++ prj/machine_learning.py
@@ -9,3 +9,17 @@ Created on Wed May  1 17:42:58 2019
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+dataset =  pd.read_csv('/home/gayathri/project/MakeComplaint/data.csv')
+
+x = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,3].values
+
+
+#Categorical data processing
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
+
