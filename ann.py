c6ba82df2683cd0b0a4e713ed2f80c744678088b
Gayathri <gaya3vs.0007@gmail.com>
2019-05-01 17:17:32 +0530

    deep learning

---
+++ ann.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 15:49:22 2019
+
+@author: gayathri
+"""
+#Ann
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('/home/gayathri/Documents/Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/ann.py')
