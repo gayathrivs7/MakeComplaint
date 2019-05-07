7aa492dbbf728ec6399a3563e2dde7a469e52c5d
gayathrivs7 <gaya3vs.0007@gmail.com>
2019-05-07 16:16:21 +0530

    trying

--- prj/machine_learning.py
+++ prj/machine_learning.py
@@ -39,7 +39,62 @@ y = label_encoder_y.fit_transform(y)
 
 label_encoder_x = LabelEncoder()
 x = label_encoder_y.fit_transform(x)
-#Encoding the complaint data
-import gensim
-from gensim.models import Word2Vec
+
+
+'''#Encoding the complaint data
+import skipthoughts
+
+class SkipThoughtsVectorizer(object):
+    def __init__(self):
+
+        self.model = skipthoughts.load_model()
+        self.encoder = skipthoughts.Encoder(self.model)
+
+    def fit_transform(self, raw_documents, y):
+    	return self.encoder.encode(raw_documents, verbose=False)
+    
+    def fit(self, raw_documents, y=None):
+    	self.fit_transform(raw_documents, y)
+    	return self
+
+    def transform(self, raw_documents, copy=True):
+    	return self.fit_transform(raw_documents, None)
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+pipeline_skipthought = Pipeline(steps=[('vectorizer', SkipThoughtsVectorizer()),
+                        ('classifier', LogisticRegression())])
+                     
+
+pipeline_tfidf = Pipeline(steps=[('vectorizer', TfidfVectorizer(ngram_range=(1, 2))),
+                        ('classifier', LogisticRegression())])
+feature_union = ('feature_union', FeatureUnion([
+    ('skipthought', SkipThoughtsVectorizer()),
+    ('tfidf', TfidfVectorizer(ngram_range=(1, 2))),
+]))
+
+
+
+pipeline_both = Pipeline(steps=[feature_union,
+                        ('classifier', LogisticRegression())])
+
+# Train and test the models
+for train_size in (10,20,30,40,50,60, len(x_train)):
+    print(train_size, '--------------------------------------')
+    # skipthought
+    pipeline_skipthought.fit(x_train[:train_size], classes_train[:train_size])
+    print ('skipthought', pipeline_skipthought.score(x_test, y_test))
+
+    # tfidf
+    pipeline_tfidf.fit(x_train[:train_size], classes_train[:train_size])
+    print('tfidf', pipeline_tfidf.score(x_test, y_test))
+
+    # both
+    pipeline_both.fit(tweets_train[:train_size], classes_train[:train_size])
+    print('skipthought+tfidf', pipeline_both.score(x_test, y_test))'''
+    
+
+
 
