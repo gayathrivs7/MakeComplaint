0ba106be1d617c597c049cad3a1cd0c84bd3b2ac
gayathrivs7 <gaya3vs.0007@gmail.com>
2019-05-18 17:04:27 +0530

    test.py spacy pos

--- prj/testdata.py
+++ prj/testdata.py
@@ -12,7 +12,7 @@ from nltk.stem import PorterStemmer
 
 def test(text_data):
     
-    text_data= text_data.replace('[^\w\s]','').lower()
+    text_data= text_data.replace('[^\w\s]','')
     porter=PorterStemmer()
     stemmer = SnowballStemmer("english")
     lemmatizer = WordNetLemmatizer()
@@ -21,7 +21,7 @@ def test(text_data):
     
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
-    for x in text_data.lower(): 
+    for x in text_data: 
         if x in punctuations: 
             text_data = text_data.replace(x, " ") 
 
@@ -34,17 +34,53 @@ def test(text_data):
     test_list =[]
 
     import spacy
+    
+  
+    #===============Removing Stop Words=================================
     spacy_nlp = spacy.load('en_core_web_sm')
     spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
-   
     for i in test_token:
-        
-        
         if i not in spacy_stopwords:
-    
             test_list.append(i)
     print("Test List")
     print(test_list)
+
+    #===============Removing NNP=================================
+    nlp = spacy.load('en')
+    nnp_list=[]
+
+    # to make spacy.tokens.token.Token
+    str =""
+    for element in test_list:
+        str+=" "+element
+    print(str)
+
+    str_tokens=[]
+    str = nlp(str)
+    for i in str:
+        str_tokens.append(i.text)
+        
+        print(type(i))
+        if i.tag_=='NNP':
+            print(type(i))
+            nnp_list.append(i.text)
+    print("NNP List")
+    print(nnp_list)
+    print("Str Tokens")
+    str_tokens.pop(0)
+    print(str_tokens)
+    
+    new_testlist=[]
+    if len(nnp_list)!=0:
+    
+        for i in str_tokens:
+            for j in nnp_list:
+                if i!=j:
+                    new_testlist.append(i)
+    print("\nNew Test List without NNP")
+    print(new_testlist)
+   
+
 
     '''New changes'''
 
